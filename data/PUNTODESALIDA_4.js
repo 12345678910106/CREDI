d132ab67e846ad8a848b1588704658e517511f1b
Charls Gons <86436696+12345678910106@users.noreply.github.com>
2021-06-25 00:23:51 -0500

    Add files via upload

---
+++ data/PUNTODESALIDA_4.js
@@ -0,0 +1,1 @@
+var json_PUNTODESALIDA_4 = {"type":"FeatureCollection","name":"PUNTODESALIDA_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[-101.241265361846359,19.685100907201669]}}]}
